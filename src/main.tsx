d309bda7e1e44fe0dcdf4d3320992f5de7cbb7e5
alexeyYashin <alexeyyashin@mail.ru>
2023-04-05 22:52:50 +0400

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM, {createRoot} from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact/>
+            }
+        ]
+    },
+
+]);
+
+const container  = document.getElementById('root') as HTMLElement
+const root = createRoot(container);
+root.render(<React.StrictMode>
+    <RouterProvider router={router} />
+</React.StrictMode>);
